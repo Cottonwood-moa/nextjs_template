1e2768314010b8fefe0b02a1f37e4ea325648e19
Cottonwood <blog_geon@naver.com>
2022-04-21 01:19:31 +0900

    add prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
+import Script from "next/script";
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig value={{ fetcher }}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
